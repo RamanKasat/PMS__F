0e4b177256990dbd8e54c5918e9af53a8973b14e
raman kasat <ramankasat1004@gmail.com>
2022-08-29 11:59:56 +0530

    pensionser-details

---
+++ process-pension-service/src/main/java/com/cts/processPension/model/PensionDetail.java
@@ -0,0 +1,29 @@
+package com.cts.processPension.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Model class for pension details
+ *
+ */
+@AllArgsConstructor
+@Getter
+public class PensionDetail {
+
+	private String name;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	private Date dateOfBirth;
+	private String pan;
+	private String pensionType;
+	private double pensionAmount;
+	private double bankCharges;
+
+	
+}
